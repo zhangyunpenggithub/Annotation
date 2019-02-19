665001a68caf48ac4aad374d9275810b3b4774f2
zhangyunpeng <zhangyunpeng@360.cn>
2019-02-19 14:50:14 +0800

    init

---
+++ app/src/main/java/com/example/annotation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.annotation
+
+import android.os.Bundle
+import android.support.annotation.MainThread
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textview.setOnClickListener {
+        }
+
+    }
+
+
+}
